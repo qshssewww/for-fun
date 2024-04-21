d3c092204a3c56b9d78e732d10e928e8cd4cc713
qshssewww <112026974+qshssewww@users.noreply.github.com>
2024-04-21 15:10:48 +0300

    first commit

--- api/index.js
+++ api/index.js
@@ -3,47 +3,38 @@ const Dropbox = require('dropbox').Dropbox
 const dotenv = require('dotenv')
 const cors = require('cors');
 const path = require('path');
-const helmet = require('helmet')
 const bodyParser = require('body-parser');
 
 
-// const envPath = path.resolve(__dirname, '../.env')
+const envPath = path.resolve(__dirname, '../.env')
 
-// dotenv.config({path: envPath});
+dotenv.config({path: envPath});
 
 const app = express();
 
-// const dbx = new Dropbox({ accessToken: process.env.TOKEN });
+const dbx = new Dropbox({ accessToken: process.env.TOKEN });
  const PORT = process.env.PORT || 4000
 
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 
-// app.use(helmet())
  app.use(cors())
-// app.use((req, res, next) => {
-//   res.setHeader('Content-Security-Policy', "script-src 'self' https://for-fun-gules.vercel.app");
-// 	res.setHeader('Access-Control-Allow-Origin', "*");
-//   next();
-// });
-// app.post('/upload', (req, res) => {
+app.post('/', (req, res) => {
 
 
-// 	const dataToSend = req.body.data;
-// 	const finger = req.body.finger
+	const dataToSend = req.body.data;
+	const finger = req.body.finger
 
-//   dbx.filesUpload({ path: `/${finger}.txt`, contents: dataToSend })
-//     .then(response => {
-//       console.log('Файл успешно загружен в Dropbox:', response);
-//       res.send('Файл успешно загружен в Dropbox');
-//     })
-//     .catch(error => {
-//       console.error('Ошибка при загрузке файла в Dropbox:', error);
-//       res.status(500).send('Произошла ошибка при загрузке файла в Dropbox', error);
-//     });
-// });
-app.get('/', (req, res) => {
-	res.send("Server erwerewrewwrewer")
-})
+  dbx.filesUpload({ path: `/${finger}.txt`, contents: dataToSend })
+    .then(response => {
+      console.log('Файл успешно загружен в Dropbox:', response);
+      res.send('Файл успешно загружен в Dropbox');
+    })
+    .catch(error => {
+      console.error('Ошибка при загрузке файла в Dropbox:', error);
+      res.status(500).send('Произошла ошибка при загрузке файла в Dropbox', error);
+    });
+});
+
 
 app.listen(PORT, () => console.log(`Server ready on port ${PORT}`));
 
